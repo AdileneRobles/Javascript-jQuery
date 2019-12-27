5a45bb8ab1cd4775fab8d0b2416dc380ecf4e5aa
AdileneRobles <58531700+AdileneRobles@users.noreply.github.com>
2019-12-26 17:37:50 -0700

    261219
    JS Objects and Prototypes

---
+++ JS OBJECTS AND PROTOTYPES/javascript-objects-prototypes/2.Js Object Properties/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*CREATE AN OBJECT*/
+//function Cat(name, color ) {
+//  this.name = name
+//  this.color = color
+//}
+
+//var cat = new Cat('Fluffy', 'White');
+
+class Cat {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+  
+  speak() {
+    display('Meeooow')
+  }
+}
+
+var cat = new Cat('Fluffy', 'White')
+
+display(cat)
+cat.speak()
